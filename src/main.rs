6f130d5e3a82fdb3e822f41e8d4a4d67429f5444
German Lashevich <german.lashevich@gmail.com>
2020-02-17 00:01:36 +0100

    Initial functional version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,91 @@
+#[macro_use]
+extern crate clap;
+extern crate postgres;
+#[macro_use]
+extern crate log;
+
+use clap::App;
+use postgres::error::SqlState;
+use postgres::{Client, NoTls};
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+#[derive(Debug)]
+struct Root {
+    word: String,
+    root: String,
+    index: usize, // can be >0 for words with multiple roots
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+    let connection_string = matches.value_of("connection-string").unwrap();
+    let input_file = matches.value_of("INPUT").unwrap();
+    let quality = matches.value_of("quality").unwrap();
+
+    let mut client =
+        Client::connect(&connection_string, NoTls).expect("Cannot connect to the database");
+
+    let reader = BufReader::new(File::open(input_file).expect("Cannot open input file"));
+
+    let insert = client
+        .prepare("INSERT INTO roots (word, root, index, quality) VALUES ($1, $2, $3, $4)")
+        .unwrap();
+    for line in reader.lines() {
+        for root in get_roots_from_string(line.unwrap().trim()) {
+            match client.execute(
+                &insert,
+                &[&root.word, &root.root, &(root.index as i32), &quality],
+            ) {
+                Ok(_) => (),
+                Err(err) => {
+                    if err.code() == Some(&SqlState::UNIQUE_VIOLATION) {
+                        warn!("Duplicate key {:?}", root);
+                        debug!("{}", err)
+                    } else {
+                        panic!("{}", err)
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn get_roots_from_string(string: &str) -> Vec<Root> {
+    let mut vec = Vec::new();
+    let parts: Vec<&str> = string.split('\t').collect();
+    let word = parts[0];
+    parts[1]
+        .split('/')
+        .filter_map(|p| {
+            let pieces: Vec<&str> = p.split(':').collect();
+            if pieces[1] == "ROOT" {
+                Some(pieces[0])
+            } else {
+                None
+            }
+        })
+        .enumerate()
+        .for_each(|x| {
+            let (index, root) = x;
+            vec.push(Root {
+                word: word.to_owned(),
+                root: root.to_owned(),
+                index,
+            })
+        });
+    vec
+}
+
+fn default_var(key: &str, default: &str) -> String {
+    match env::var(key) {
+        Ok(val) => val,
+        Err(err) => match err {
+            env::VarError::NotPresent => default.to_owned(),
+            env::VarError::NotUnicode(_) => panic!(err),
+        },
+    }
 }
